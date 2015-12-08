c099a1d76457cba710e802947a98ecd119227bb4
jamesKao <vir778352@gmail.com>
2015-12-08 23:00:34 +0800

    修該資料夾名稱

--- slideWithPlugIn/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var jsonImageSrc = {
-        "picfolder": "./img/",
-        "data": ['slide-1.jpg', 'slide-2.jpg', 'slide-3.jpg', 'slide-4.jpg']
-    };
-
-    $('.slideshow').slide(jsonImageSrc);
-});
